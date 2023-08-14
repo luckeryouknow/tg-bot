73883309ec9e7a55a5fdd4816da85d51c1254cd5
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2023-08-14 16:24:37 +0300

    Charts list added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import telebot
 from telebot import types
-from inline_buttons_handler import handle_next_currency_button, handle_next_price_button, handle_previous_currency_button, handle_previous_price_button
+from inline_buttons_handler import handle_next_currency_button, handle_next_price_button, handle_previous_currency_button, handle_previous_price_button, handle_chart_button
 from commands_handler import commands_handler
 
-API_TOKEN = '6388083417:AAFnoBZpLQkrrF95Bj9uq0nYma5EUt9qs1k'
+API_TOKEN = '6122625919:AAHRcg7YSpfzAIsABv0afJbp3GgmAwfQnQs'
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -17,6 +17,8 @@ markup.add(button1, button2, button3)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Hi, {0.first_name}'.format(message.from_user), reply_markup=markup)
+
+
 
 @bot.message_handler(content_types='text')
 def commands_wrapper(message):
@@ -38,4 +40,8 @@ def previous_pice_wrapper(calc):
 def next_price_wrapper(calc):
     handle_next_price_button(calc)
 
+@bot.callback_query_handler(func=lambda call: call.data.startswith('chart'))
+def chart_wrapper(message):
+    handle_chart_button(message)
+    
 bot.polling(none_stop=True)
